d8d20132d5a4e9c3300fd2dea895452e5662a36d
Robert Brockie <robertbrockie@gmail.com>
2014-02-15 14:51:25 -0500

    Simple inital design

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Buchans Enterprises</title>
+		<link href="/css/style.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<div class="fullscreen">
+			<div class="card">
+				<h1>Buchans Enterprises LTD.</h1>
+				<p>Michael O'Brien, <i>B Eng., Mechanical Engineer</i></p>
+				<p>Tel/Fax: (709) 672-3871</p>
+				<p>Cell: (709) 672-7081</p>
+				<p>Email: paulobrien@nf.aibn.com</p>
+			</div>
+		</div>
+	</body>
+</html>
+
